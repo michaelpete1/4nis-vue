98a49322fa9a12592e625698f8925d9d3e14917b
Chidi Onyekwere <michaelpet40@gmail.com>
2025-05-06 16:32:05 +0100

    changed some things

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/home.vue";
+import Home from "../views/Home.vue";
 import About from "../views/about.vue";
 import Products from "../views/products.vue";
 import Contact from "../views/contact.vue";
 import Settings from "../views/settings.vue";
 
 const routes = [
-  { path: "/", name: "home", component: Home },
+  { path: "/", name: "Home", component: Home },
   { path: "/about", name: "about", component: About },
   { path: "/products", name: "products", component: Products },
   { path: "/contact", name: "contact", component: Contact },
