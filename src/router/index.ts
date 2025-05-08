6315cff055d3a63b250f37f494d6c535646c2e88
Chidi Onyekwere <michaelpet40@gmail.com>
2025-05-08 10:55:43 +0100

    changed some things

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/about.vue";
-import Products from "../views/products.vue";
-import Contact from "../views/contact.vue";
-import Settings from "../views/settings.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+
+import About from '../views/about.vue';
+import Products from '../views/products.vue';
+import Contact from '../views/contact.vue';
+import Settings from '../views/settings.vue';
+import Home from '../views/Home.vue'; // Make sure the casing matches the file name exactly
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "about", component: About },
-  { path: "/products", name: "products", component: Products },
-  { path: "/contact", name: "contact", component: Contact },
-  { path: "/settings", name: "settings", component: Settings },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/about', name: 'about', component: About },
+  { path: '/products', name: 'products', component: Products },
+  { path: '/contact', name: 'contact', component: Contact },
+  { path: '/settings', name: 'settings', component: Settings },
 ];
 
 const router = createRouter({
